46490048b72401628c6d7aca883a6826ddbfc442
pioka <20146647+pioka@users.noreply.github.com>
2023-12-03 22:04:43 +0900

    genshin-via-steam-input: コード整形

--- genshin-via-steam-input/genshin-via-steam-input.ps1
+++ genshin-via-steam-input/genshin-via-steam-input.ps1
@@ -1,28 +1,20 @@
 # Steamを閉じる
-Start-Process -Wait `
-  'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown'
-
+Start-Process 'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown' -Wait
 while (Get-Process -Name steam -ErrorAction SilentlyContinue) {
   Start-Sleep 1
-  echo 'Waiting for Steam to shutdown...'
 }
 
 # 管理者としてSteamを立ち上げ,原神を起動
-Start-Process -Verb RunAs `
-  'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent steam://rungameid/xxxxxxxxxxxxxxxxxxxx'
+Start-Process 'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent steam://rungameid/xxxxxxxxxxxxxxxxxxxx' -Verb RunAs 
 
-# ゲーム終了後にEnterを押させる想定
-pause
+# ゲーム終了後にEnterを押させる
+Read-Host -Prompt "ゲームが終了したらEnterを押してね"
 
 # 管理者として立ち上げたSteamを閉じる
-Start-Process -Wait `
-  'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown'
-
+Start-Process 'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown' -Wait
 while (Get-Process -Name steam -ErrorAction SilentlyContinue) {
   Start-Sleep 1
-  echo 'Waiting for Steam to shutdown...'
 }
 
 # 元のユーザーでSteamを立ち上げる
-Start-Process -Wait `
-  'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent'
+Start-Process 'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent'
