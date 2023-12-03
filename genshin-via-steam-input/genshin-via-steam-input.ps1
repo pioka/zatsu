d8810c9d02e3e45288264ab8327f231c5d9d21f6
pioka <20146647+pioka@users.noreply.github.com>
2023-12-03 21:48:40 +0900

    genshin-via-steam-input: Steamの終了を待つようにする

--- genshin-via-steam-input/genshin-via-steam-input.ps1
+++ genshin-via-steam-input/genshin-via-steam-input.ps1
@@ -2,6 +2,11 @@
 Start-Process -Wait `
   'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown'
 
+while (Get-Process -Name steam -ErrorAction SilentlyContinue) {
+  Start-Sleep 1
+  echo 'Waiting for Steam to shutdown...'
+}
+
 # 管理者としてSteamを立ち上げ,原神を起動
 Start-Process -Verb RunAs `
   'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent steam://rungameid/xxxxxxxxxxxxxxxxxxxx'
@@ -9,10 +14,15 @@ Start-Process -Verb RunAs `
 # ゲーム終了後にEnterを押させる想定
 pause
 
-# Steamを閉じる
+# 管理者として立ち上げたSteamを閉じる
 Start-Process -Wait `
   'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent -shutdown'
 
-# 元のユーザーで再度Steamを立ち上げる
-Start-Process `
+while (Get-Process -Name steam -ErrorAction SilentlyContinue) {
+  Start-Sleep 1
+  echo 'Waiting for Steam to shutdown...'
+}
+
+# 元のユーザーでSteamを立ち上げる
+Start-Process -Wait `
   'C:\Program Files (x86)\Steam\steam.exe' -ArgumentList '-silent'
